28fb1380bf8826366e2e2fcd5ff33b058d6a45c0
hamigakitanuki <55008212+hamigakitanuki@users.noreply.github.com>
2024-10-27 16:52:56 +0900

    Revert "feat:タスクNo 001対応"

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 def greet():
     print("Hello, GitHub Workshop!")
 
-# farewell関数の追加
-def farewell():
-    print("Goodbye, GitHub Workshop!")
-
 if __name__ == "__main__":
     greet()
-    farewell()
+
